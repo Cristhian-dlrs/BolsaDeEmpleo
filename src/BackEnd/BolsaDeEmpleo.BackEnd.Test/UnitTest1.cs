bb0ab1ad3f51bbc0e080a1a6fc4a404044b93a1d
Cristhian-dlrs <christian.dlr.ing@gmail.com>
2021-10-09 13:40:52 -0400

    Initial commit

---
+++ src/BackEnd/BolsaDeEmpleo.BackEnd.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace BolsaDeEmpleo.BackEnd.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
